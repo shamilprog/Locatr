9dd5d2f327482e08493b1d56274131993a1ab480
Shamilprog <shamilprog@yandex.ru>
2021-01-12 21:57:56 +0300

    Add some helper methods into utils classes to work with locations.

--- app/src/main/java/ru/shamilprog/android/locatr/FlickrFetchr.java
+++ app/src/main/java/ru/shamilprog/android/locatr/FlickrFetchr.java
@@ -1,5 +1,6 @@
 package ru.shamilprog.android.locatr;
 
+import android.location.Location;
 import android.net.Uri;
 import android.util.Log;
 
@@ -67,6 +68,11 @@ public class FlickrFetchr {
         return downloadGalleryItems(url);
     }
 
+    public List<GalleryItem> searchPhotos(Location location) {
+        String url = buildUrl(location);
+        return downloadGalleryItems(url);
+    }
+
     private List<GalleryItem> downloadGalleryItems(String url) {
         List<GalleryItem> items = new ArrayList<>();
 
@@ -93,6 +99,14 @@ public class FlickrFetchr {
         }
 
         return uriBuilder.build().toString();
+    }
+
+    private String buildUrl(Location location) {
+        return ENDPOINT.buildUpon()
+                .appendQueryParameter("method", SEARCH_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build().toString();
     }
 
     private void parseItems(List<GalleryItem> items, JSONObject jsonBody)
