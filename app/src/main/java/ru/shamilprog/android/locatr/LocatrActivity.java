b92fe29d63d47bd836b176f0ff905d182c1aa3a4
Shamilprog <shamilprog@yandex.ru>
2021-01-12 21:13:56 +0300

    Building application's interface

--- app/src/main/java/ru/shamilprog/android/locatr/LocatrActivity.java
+++ app/src/main/java/ru/shamilprog/android/locatr/LocatrActivity.java
@@ -1,13 +1,13 @@
 package ru.shamilprog.android.locatr;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LocatrActivity extends AppCompatActivity {
+public class LocatrActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return LocatrFragment.newInstance();
     }
 }
