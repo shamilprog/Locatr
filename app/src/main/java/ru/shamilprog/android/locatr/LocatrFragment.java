d164ad1396c8877df2d005f7216afdc101976801
Shamilprog <shamilprog@yandex.ru>
2021-01-12 22:17:30 +0300

    Fetching location data.

--- app/src/main/java/ru/shamilprog/android/locatr/LocatrFragment.java
+++ app/src/main/java/ru/shamilprog/android/locatr/LocatrFragment.java
@@ -1,9 +1,11 @@
 package ru.shamilprog.android.locatr;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,9 +15,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class LocatrFragment extends Fragment {
+    private static final String TAG = "LocatrFragment";
+
     private ImageView mImageView;
     private GoogleApiClient mClient;
 
@@ -76,5 +82,30 @@ public class LocatrFragment extends Fragment {
 
         MenuItem searchItem = menu.findItem(R.id.action_locate);
         searchItem.setEnabled(mClient.isConnected());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_locate:
+                findImage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void findImage() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got a fix: " + location);
+                    }
+                });
     }
 }
