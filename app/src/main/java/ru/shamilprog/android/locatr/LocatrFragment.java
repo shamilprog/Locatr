15bd28f8a1e6239755c2bacb71c6e68580e3e35c
Shamilprog <shamilprog@yandex.ru>
2021-01-15 00:06:04 +0300

    Show a map in the app.

--- app/src/main/java/ru/shamilprog/android/locatr/LocatrFragment.java
+++ app/src/main/java/ru/shamilprog/android/locatr/LocatrFragment.java
@@ -24,11 +24,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.io.IOException;
 import java.util.List;
 
-public class LocatrFragment extends Fragment {
+public class LocatrFragment extends SupportMapFragment {
     private static final String TAG = "LocatrFragment";
     private static final String[] LOCATION_PERMISSIONS = new String[] {
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -36,7 +37,6 @@ public class LocatrFragment extends Fragment {
     };
     private static final int REQUEST_LOCATION_PERMISSIONS = 0;
 
-    private ImageView mImageView;
     private GoogleApiClient mClient;
 
     public static LocatrFragment newInstance() {
@@ -64,15 +64,6 @@ public class LocatrFragment extends Fragment {
                 .build();
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_locatr, container, false);
-
-        mImageView = (ImageView) v.findViewById(R.id.image);
-
-        return v;
-    }
 
     @Override
     public void onStart() {
@@ -175,7 +166,7 @@ public class LocatrFragment extends Fragment {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            mImageView.setImageBitmap(mBitmap);
+
         }
     }
 }
