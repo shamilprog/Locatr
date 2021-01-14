d67a39aff21eb365b3df17602b76853a30399193
Shamilprog <shamilprog@yandex.ru>
2021-01-14 06:55:27 +0300

    Add API KEY that was missed.

--- app/src/main/java/ru/shamilprog/android/locatr/FlickrFetchr.java
+++ app/src/main/java/ru/shamilprog/android/locatr/FlickrFetchr.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class FlickrFetchr {
     private static final String TAG = "FlickrFetchr";
 
-    private static final String API_KEY = "REPLACE_ME_WITH_A_REAL_KEY";
+    private static final String API_KEY = "578520656cb878a23df255c74a505c03";
     private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
     private static final String SEARCH_METHOD = "flickr.photos.search";
     private static final Uri ENDPOINT = Uri
